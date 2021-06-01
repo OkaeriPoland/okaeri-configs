90e775b3b84c70e947f2260b9fc66c1883f2f7d1
PinkLolicorn <pinklolicorngithub@daffit.pl>
2021-06-01 09:18:00 +0200

    Allow DeserializationData#configurer access, release 2.7.19

--- core/src/main/java/eu/okaeri/configs/serdes/DeserializationData.java
+++ core/src/main/java/eu/okaeri/configs/serdes/DeserializationData.java
@@ -3,6 +3,7 @@ package eu.okaeri.configs.serdes;
 import eu.okaeri.configs.configurer.Configurer;
 import eu.okaeri.configs.schema.GenericsDeclaration;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -13,7 +14,7 @@ import java.util.Map;
 public class DeserializationData {
 
     private Map<String, Object> data;
-    private Configurer configurer;
+    @Getter private Configurer configurer;
 
     public Map<String, Object> asMap() {
         return this.data;
