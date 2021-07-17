cdab783459c7de6c6711066e5f376b40cff43c91
PinkLolicorn <pinklolicorngithub@daffit.pl>
2021-07-17 22:29:09 +0200

    Register StringPotionEffectTypeTransformer in SerdesBukkit (Fixes #9)

--- serdes-bukkit/src/main/java/eu/okaeri/configs/yaml/bukkit/serdes/SerdesBukkit.java
+++ serdes-bukkit/src/main/java/eu/okaeri/configs/yaml/bukkit/serdes/SerdesBukkit.java
@@ -1,13 +1,14 @@
 package eu.okaeri.configs.yaml.bukkit.serdes;
 
+import eu.okaeri.configs.serdes.OkaeriSerdesPack;
+import eu.okaeri.configs.serdes.TransformerRegistry;
 import eu.okaeri.configs.yaml.bukkit.serdes.serializer.ItemMetaSerializer;
 import eu.okaeri.configs.yaml.bukkit.serdes.serializer.ItemStackSerializer;
 import eu.okaeri.configs.yaml.bukkit.serdes.serializer.LocationSerializer;
 import eu.okaeri.configs.yaml.bukkit.serdes.serializer.PotionEffectSerializer;
 import eu.okaeri.configs.yaml.bukkit.serdes.transformer.StringEnchantmentTransformer;
+import eu.okaeri.configs.yaml.bukkit.serdes.transformer.StringPotionEffectTypeTransformer;
 import eu.okaeri.configs.yaml.bukkit.serdes.transformer.StringWorldTransformer;
-import eu.okaeri.configs.serdes.OkaeriSerdesPack;
-import eu.okaeri.configs.serdes.TransformerRegistry;
 
 public class SerdesBukkit implements OkaeriSerdesPack {
 
@@ -21,7 +22,8 @@ public class SerdesBukkit implements OkaeriSerdesPack {
         registry.register(new PotionEffectSerializer());
 
         // transformers
-        registry.register(new StringWorldTransformer());
         registry.register(new StringEnchantmentTransformer());
+        registry.register(new StringPotionEffectTypeTransformer());
+        registry.register(new StringWorldTransformer());
     }
 }
