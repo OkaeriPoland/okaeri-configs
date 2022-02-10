062a4a15afd9fc9cf2fab5da75c73fef42d86c83
Sandra <sandra@okaeri.cloud>
2022-02-10 03:05:00 +0100

    Bump okaeri-validator to 2.0.1, release 4.0.0-beta5

--- serdes-bucket4j/README.md
+++ serdes-bucket4j/README.md
@@ -20,7 +20,7 @@ Add dependency to the `dependencies` section:
 <dependency>
   <groupId>eu.okaeri</groupId>
   <artifactId>okaeri-configs-serdes-bucket4j</artifactId>
-  <version>4.0.0-beta3</version>
+  <version>4.0.0-beta5</version>
 </dependency>
 ```
 
@@ -29,7 +29,7 @@ Add dependency to the `dependencies` section:
 Add dependency to the `maven` section:
 
 ```groovy
-implementation 'eu.okaeri:okaeri-configs-serdes-bucket4j:4.0.0-beta3'
+implementation 'eu.okaeri:okaeri-configs-serdes-bucket4j:4.0.0-beta5'
 ```
 
 ## Supported types
--- validator-okaeri/src/main/java/eu/okaeri/configs/validator/okaeri/OkaeriValidator.java
+++ validator-okaeri/src/main/java/eu/okaeri/configs/validator/okaeri/OkaeriValidator.java
@@ -8,12 +8,13 @@ import eu.okaeri.validator.ConstraintViolation;
 import eu.okaeri.validator.policy.NullPolicy;
 import lombok.NonNull;
 
+import java.lang.reflect.Field;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class OkaeriValidator extends WrappedConfigurer {
 
-    private final boolean defaultNotNull;
+    private final eu.okaeri.validator.OkaeriValidator validator;
 
     public OkaeriValidator(@NonNull Configurer wrapped) {
         this(wrapped, false);
@@ -21,18 +22,15 @@ public class OkaeriValidator extends WrappedConfigurer {
 
     public OkaeriValidator(@NonNull Configurer wrapped, boolean defaultNotNull) {
         super(wrapped);
-        this.defaultNotNull = defaultNotNull;
+        this.validator = eu.okaeri.validator.OkaeriValidator.of(defaultNotNull ? NullPolicy.NOT_NULL : NullPolicy.NULLABLE);
     }
 
     @Override
     public boolean isValid(@NonNull FieldDeclaration declaration, Object value) {
 
         Class<?> parent = declaration.getObject().getClass();
-        String realFieldName = declaration.getField().getName();
-
-        NullPolicy nullPolicy = this.defaultNotNull ? NullPolicy.NOT_NULL : NullPolicy.NULLABLE;
-        eu.okaeri.validator.OkaeriValidator validator = eu.okaeri.validator.OkaeriValidator.of(parent, nullPolicy);
-        Set<ConstraintViolation> violations = validator.validateValue(realFieldName, value);
+        Field field = declaration.getField();
+        Set<ConstraintViolation> violations = this.validator.validatePropertyValue(parent, field, value);
 
         if (!violations.isEmpty()) {
             String reason = violations.stream().map(ConstraintViolation::getMessage).collect(Collectors.joining(", "));
