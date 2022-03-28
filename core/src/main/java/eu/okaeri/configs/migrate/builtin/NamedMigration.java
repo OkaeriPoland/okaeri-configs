a8bf63ab848f98c14e2fd16f2c8d4ad0d64b3032
Sandra <sandra@okaeri.cloud>
2022-03-28 09:37:32 +0200

    Add any/all special dsl methods for migrations, release 4.0.0-beta14

--- core/src/main/java/eu/okaeri/configs/migrate/builtin/NamedMigration.java
+++ core/src/main/java/eu/okaeri/configs/migrate/builtin/NamedMigration.java
@@ -1,12 +1,12 @@
 package eu.okaeri.configs.migrate.builtin;
 
 import eu.okaeri.configs.migrate.ConfigMigration;
-import eu.okaeri.configs.migrate.builtin.special.SimpleSequentialMigration;
+import eu.okaeri.configs.migrate.builtin.special.SimpleMultiMigration;
 import lombok.Getter;
 import lombok.ToString;
 
 @ToString
-public class NamedMigration extends SimpleSequentialMigration {
+public class NamedMigration extends SimpleMultiMigration {
 
     private @Getter final String description;
 
