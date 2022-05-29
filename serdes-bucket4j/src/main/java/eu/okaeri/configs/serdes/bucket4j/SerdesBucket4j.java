b3a45d40e986fad4bfc3089f0f8efdf806bcef11
PinkLolicorn <pinklolicorngithub@daffit.pl>
2022-05-30 01:07:11 +0200

    Add support for BucketConfiguration to serdes-bucket4j, release 4.0.0-beta22

--- serdes-bucket4j/src/main/java/eu/okaeri/configs/serdes/bucket4j/SerdesBucket4j.java
+++ serdes-bucket4j/src/main/java/eu/okaeri/configs/serdes/bucket4j/SerdesBucket4j.java
@@ -3,7 +3,7 @@ package eu.okaeri.configs.serdes.bucket4j;
 import eu.okaeri.configs.serdes.OkaeriSerdesPack;
 import eu.okaeri.configs.serdes.SerdesRegistry;
 import eu.okaeri.configs.serdes.bucket4j.serializer.BandwidthSerializer;
-import eu.okaeri.configs.serdes.bucket4j.serializer.LocalBucketSerializer;
+import eu.okaeri.configs.serdes.bucket4j.serializer.BucketConfigurationSerializer;
 import eu.okaeri.configs.serdes.bucket4j.serializer.SingleBandwidthBucketSerializer;
 import lombok.NonNull;
 
@@ -12,7 +12,7 @@ public class SerdesBucket4j implements OkaeriSerdesPack {
     @Override
     public void register(@NonNull SerdesRegistry registry) {
         registry.register(new BandwidthSerializer());
-        registry.register(new LocalBucketSerializer());
+        registry.register(new BucketConfigurationSerializer());
         registry.register(new SingleBandwidthBucketSerializer());
     }
 }
