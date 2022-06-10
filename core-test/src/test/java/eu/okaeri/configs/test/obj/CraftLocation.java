49a9f546c1ec2e5d4a8cd6fa7db6091f7c9a9f50
PinkLolicorn <pinklolicorngithub@daffit.pl>
2022-06-11 00:52:40 +0200

    Remove old tests

--- core-test/src/test/java/eu/okaeri/configs/test/obj/CraftLocation.java
+++
@@ -1,7 +0,0 @@
-package eu.okaeri.configs.test.obj;
-
-public class CraftLocation extends Location {
-    public CraftLocation(World world, double x, double y, double z, float pitch, float yaw) {
-        super(world, x, y, z, pitch, yaw);
-    }
-}
