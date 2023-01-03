6d52cb485a35b202d6eb59c0e9c2f470a804bed9
PinkLolicorn <pinklolicorngithub@daffit.pl>
2023-01-03 19:35:09 +0100

    Add updateFunction to move dsl

--- core/src/main/java/eu/okaeri/configs/migrate/builtin/action/SimpleMoveMigration.java
+++ core/src/main/java/eu/okaeri/configs/migrate/builtin/action/SimpleMoveMigration.java
@@ -3,16 +3,21 @@ package eu.okaeri.configs.migrate.builtin.action;
 import eu.okaeri.configs.OkaeriConfig;
 import eu.okaeri.configs.migrate.ConfigMigration;
 import eu.okaeri.configs.migrate.view.RawConfigView;
+import lombok.AllArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
+import java.util.function.Function;
+
 @ToString
+@AllArgsConstructor
 @RequiredArgsConstructor
 public class SimpleMoveMigration implements ConfigMigration {
 
     private final String fromKey;
     private final String toKey;
+    private Function<Object, Object> updateFunction;
 
     @Override
     public boolean migrate(@NonNull OkaeriConfig config, @NonNull RawConfigView view) {
@@ -22,8 +27,13 @@ public class SimpleMoveMigration implements ConfigMigration {
         }
 
         Object targetValue = view.remove(this.fromKey);
-        Object oldValue = view.set(this.toKey, targetValue);
+        if (this.updateFunction == null) {
+            view.set(this.toKey, targetValue);
+            return true;
+        }
 
+        Object updatedValue = this.updateFunction.apply(targetValue);
+        view.set(this.toKey, updatedValue);
         return true;
     }
 }
