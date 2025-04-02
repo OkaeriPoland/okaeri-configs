d623ddbaa49a8a5e905323c6f710a9db3ceb43f6
Sandra <sandra@okaeri.cloud>
2025-04-02 23:53:12 +0200

    Reimplement byName resolving for Enchantment/PotionEffectType - "Fixes" Enchantment#getByName(String) returning nulls for Enchantment#getName() not matching namespace key

--- serdes-bukkit/src/main/java/eu/okaeri/configs/yaml/bukkit/serdes/transformer/StringEnchantmentTransformer.java
+++ serdes-bukkit/src/main/java/eu/okaeri/configs/yaml/bukkit/serdes/transformer/StringEnchantmentTransformer.java
@@ -7,10 +7,12 @@ import eu.okaeri.configs.serdes.SerdesContext;
 import lombok.NonNull;
 import org.bukkit.enchantments.Enchantment;
 
-import java.util.Arrays;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class StringEnchantmentTransformer extends BidirectionalTransformer<String, Enchantment> {
+
+    private static Map<String, Enchantment> byName = new HashMap<>();
 
     @Override
     public GenericsPair<String, Enchantment> getPair() {
@@ -20,6 +22,18 @@ public class StringEnchantmentTransformer extends BidirectionalTransformer<Strin
     @Override
     public Enchantment leftToRight(@NonNull String data, @NonNull SerdesContext serdesContext) {
         Enchantment enchantment = Enchantment.getByName(data);
+        if (enchantment == null) {
+            if (byName.isEmpty()) {
+                // At some point the byName map from Enchantment was removed and getByName
+                // was reimplemented as resolve by the enchantment key causing the following:
+                // - UNBREAKING.getName() -> DURABILITY
+                // - getByName("DURABILITY") -> null
+                for (Enchantment e : Enchantment.values()) {
+                    byName.put(e.getName().toUpperCase(Locale.ROOT), e);
+                }
+            }
+            enchantment = byName.get(data.toUpperCase(Locale.ROOT));
+        }
         if (enchantment == null) {
             String available = Arrays.stream(Enchantment.values()).map(Enchantment::getName).collect(Collectors.joining(", "));
             throw new OkaeriException("Unknown enchantment: " + data + " (Available: " + available);
