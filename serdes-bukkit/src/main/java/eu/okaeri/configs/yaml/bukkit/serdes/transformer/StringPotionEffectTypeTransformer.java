a669df07242e9449c2b6f7b188b27b6360c9ae01
PinkLolicorn <pinklolicorngithub@daffit.pl>
2025-04-02 23:53:12 +0200

    Reimplement byName resolving for Enchantment/PotionEffectType - "Fixes" Enchantment#getByName(String) returning nulls for Enchantment#getName() not matching namespace key

--- serdes-bukkit/src/main/java/eu/okaeri/configs/yaml/bukkit/serdes/transformer/StringPotionEffectTypeTransformer.java
+++ serdes-bukkit/src/main/java/eu/okaeri/configs/yaml/bukkit/serdes/transformer/StringPotionEffectTypeTransformer.java
@@ -8,9 +8,14 @@ import lombok.NonNull;
 import org.bukkit.potion.PotionEffectType;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class StringPotionEffectTypeTransformer extends BidirectionalTransformer<String, PotionEffectType> {
+
+    private static Map<String, PotionEffectType> byName = new HashMap<>();
 
     @Override
     public GenericsPair<String, PotionEffectType> getPair() {
@@ -20,6 +25,14 @@ public class StringPotionEffectTypeTransformer extends BidirectionalTransformer<
     @Override
     public PotionEffectType leftToRight(@NonNull String data, @NonNull SerdesContext serdesContext) {
         PotionEffectType potionEffectType = PotionEffectType.getByName(data);
+        if (potionEffectType == null) {
+            if (byName.isEmpty()) {
+                for (PotionEffectType p : PotionEffectType.values()) {
+                    byName.put(p.getName().toUpperCase(Locale.ROOT), p);
+                }
+            }
+            potionEffectType = byName.get(data.toUpperCase(Locale.ROOT));
+        }
         if (potionEffectType == null) {
             String available = Arrays.stream(PotionEffectType.values()).map(PotionEffectType::getName).collect(Collectors.joining(", "));
             throw new OkaeriException("Unknown potion effect type: " + data + " (Available: " + available + ")");
