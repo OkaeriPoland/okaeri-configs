8cb39807ff527ba6b3a902c1ceae7d7012b705ed
Sandra <sandra@okaeri.cloud>
2025-10-15 23:26:13 +0200

    Remove old tests

--- core-test/src/test/java/eu/okaeri/configs/test/old/impl/TestConfigParent.java
+++
@@ -1,7 +0,0 @@
-package eu.okaeri.configs.test.old.impl;
-
-import eu.okaeri.configs.OkaeriConfig;
-
-public class TestConfigParent extends OkaeriConfig {
-    private String fromParent = "hi";
-}
--- core-test/src/test/java/eu/okaeri/configs/test/old/obj/CraftLocation.java
+++
@@ -1,7 +0,0 @@
-package eu.okaeri.configs.test.old.obj;
-
-public class CraftLocation extends Location {
-    public CraftLocation(World world, double x, double y, double z, float pitch, float yaw) {
-        super(world, x, y, z, pitch, yaw);
-    }
-}
