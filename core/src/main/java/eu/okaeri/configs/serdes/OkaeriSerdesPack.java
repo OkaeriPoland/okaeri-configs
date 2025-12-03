e1ae4006b7bcefc0738e72443f41878cdb2626b8
Sandra <sandra@okaeri.cloud>
2025-12-03 22:29:14 +0100

    feat: add deprecated OkaeriSerdesPack alias for compatibility

---
+++ core/src/main/java/eu/okaeri/configs/serdes/OkaeriSerdesPack.java
@@ -0,0 +1,9 @@
+package eu.okaeri.configs.serdes;
+
+/**
+ * @deprecated Renamed to {@link OkaeriSerdes}. Provided for binary compatibility with 6.x only.
+ * May be removed at any time. Update your code to use {@link OkaeriSerdes} directly.
+ */
+@Deprecated
+public interface OkaeriSerdesPack extends OkaeriSerdes {
+}
